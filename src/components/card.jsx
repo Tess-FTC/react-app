032b930d63c5a384f57678d859d0f7887fd7f209
Tess Huang <tess@freshtrackscanada.com>
2019-08-09 09:31:44 -0700

    refine the getTrip func to be async func

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,18 +3,18 @@ import React, { Component } from 'react';
 class Card extends Component {
   state = {  }
   render() { 
-    const { imgUrl, duration, name, url, placeVisited, priceCadHigh } = this.props;
+    const { imgUrl, duration, name, url, placeVisited, priceCadLow } = this.props;
     return ( 
-        <div className="card" style={{ width: '30%' }}>
-          <img src={imgUrl} className="card-img-top" alt={name} />
+        <a className="card" style={{ width: '33.333%', textDecoration: 'none', color: '#000'}} href={url}>
+          <img src={imgUrl} className="card-img-top" alt={name} style={{ height:'calc(16/9 * 9vw)' }}/>
           <div className="card-body">
             <h5>{duration} Days</h5>
             <h3 className="card-title">{name}</h3>
             <p className="card-text">{placeVisited}</p>
-            <p>{priceCadHigh}</p>
+            <p>From ${priceCadLow}</p>
             <a href={url} className="btn btn-danger">See Details</a>
           </div>
-        </div>
+        </a>
      );
   }
 }
--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Card from './card';
+import axios from 'axios';
 
 class Cards extends Component {
 	state = { 
@@ -7,28 +8,23 @@ class Cards extends Component {
 	 }
 	
 	componentDidMount() {
-		const axios = require('axios');
-
-		axios.get('https://canadavacations.com/wp-json/wp/v2/find-trips/')
-		.then( response => {
-			console.log( typeof(response.data));
-			const tripData = response.data; 
-				this.setState({
-					cards: tripData
-				})
-		})
-		.catch(function (error) {
-			console.log(error);
-		})
-		.then((response) => {
-			// always executed
-		});  
-	
-	// Want to use async/await? Add the `async` keyword to your outer function/method.
-
+		this.getTrip();
 	}
 	
-
+	// Want to use async/await? Add the `async` keyword to your outer function/method.
+	getTrip = async () => { 
+		try {
+			const response =  await axios.get('https://canadavacations.com/wp-json/wp/v2/find-trips/');
+			console.log(response.data);
+			const cards = response.data;
+			this.setState({ cards })
+		} catch(error) {
+			console.error(error);
+		}
+	}
+	handleSorting = () => {
+		
+	}
 	render() { 
 		console.log(this.state.cards);
 		return(
@@ -41,7 +37,7 @@ class Cards extends Component {
 						name={card.name}
 						url={card.url}
 						placesVisited={card.places_visited}
-						priceCadHigh={card.price_cad_high}
+						priceCadLow={card.price_cad_low}
 					/>
 				))}
 			</div>
